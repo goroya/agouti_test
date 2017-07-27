21e289d08a3df7f171ffaa564bf1a38297a72b83
goroya <goroyahoo@gmail.com>
2017-07-28 01:33:42 +0900

    first commit

--- main.go
+++ main.go
@@ -36,12 +36,16 @@ func main() {
 		log.Fatalf("Failed to open page:%v", err)
 	}
 
-	if err := page.Navigate("https://www.w3schools.com/html/html_examples.asp"); err != nil {
+	if err := page.Navigate("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe"); err != nil {
 		log.Fatalf("Failed to navigate:%v", err)
 	}
 	time.Sleep(1 * time.Second)
-	fmt.Println(page.Find("#footer"))
+	fmt.Println(page.Find("#iframeResult").SwitchToFrame())
+	time.Sleep(3 * time.Second)
+	fmt.Println(page.Find("body > iframe").SwitchToFrame())
+	time.Sleep(3 * time.Second)
+	fmt.Println(page.Find("body > div.w3-hide-large.w3-hide-large.w3-padding-16 > div:nth-child(1) > a").Text())
 	fmt.Println("-------")
 
-	//page.Screenshot("./test.jpg")
+	page.Screenshot("./test.jpg")
 }
